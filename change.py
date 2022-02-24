9f2c374e292f9377a856be63a931d9e137c8cd95
lumokato <suzto@qq.com>
2022-02-24 11:45:29 +0800

    1

--- change.py
+++ change.py
@@ -7,7 +7,7 @@ def change_damage():
     lst = os.listdir('qd/1/')
     for file in lst:
         data = pd.read_csv('qd/1/' + file, index_col=0)
-        for i in range(data.shape[1]):
+        for i in range(data.shape[0]):
             status = clanbattle.boss_status(data.loc[i, 'damage'])
             data.loc[i:, ('lap', 'boss_id', 'remain')] = [status[0], status[1], status[2]]
         df = pd.DataFrame(data)
