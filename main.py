fdd21a7b827685081628a9b4694485967ea50d64
lumokato <suzto@qq.com>
2022-06-23 17:07:08 +0800

    main

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ def move_data():
     dir = os.getcwd()
     if not os.path.exists(dir+'/qd/history/1/'):
         os.makedirs(dir+'/qd/history/1')
-    shutil.copyfile(os.path.join(dir+'/qd/1', os.listdir(dir+'/qd/1')[-1]), dir+'/qd/history/1/'+str(year)+'年'+str(int(month)-1)+'月.csv')
-    shutil.move(dir+'/qd/1', dir+'/qd/history/'+year[-2:]+'-'+'{:0>2d}'.format(int(month)-1))
-    os.makedirs(dir+'/qd/1')
-    print('o')
+    if os.listdir(dir+'/qd/1'):
+        shutil.copyfile(os.path.join(dir+'/qd/1', os.listdir(dir+'/qd/1')[-1]), dir+'/qd/history/1/'+str(year)+'年'+str(int(month)-1)+'月.csv')
+        shutil.move(dir+'/qd/1', dir+'/qd/history/'+year[-2:]+'-'+str(int(month)-1))
+        os.makedirs(dir+'/qd/1')
 
 
 def monthly(scheduler):
@@ -29,10 +29,10 @@ def monthly(scheduler):
     if clan_time:
         scheduler.add_job(move_data, 'date', run_date=clan_time[0]-datetime.timedelta(hours=5))
         scheduler.add_job(clanbattle.stage_data, 'interval', minutes=30, start_date=clan_time[0]+datetime.timedelta(minutes=1), end_date=clan_time[1])
+        scheduler.add_job(clanbattle.stage_data, 'date', run_date=clan_time[1]+datetime.timedelta(days=7))
 
 
 if __name__ == '__main__':
-    move_data()
     scheduler = BlockingScheduler(timezone="Asia/Shanghai")
-    scheduler.add_job(monthly, 'cron', day='23', hour='17', args=[scheduler])
+    scheduler.add_job(monthly, 'cron', day='23', hour='18', args=[scheduler])
     scheduler.start()
